b38f0cae4a4308fa4737c45b1ad07b9140175766
Abu-Sayad-Hussain <abusayadhussain@gmail.com>
2016-10-13 14:07:10 +0600

    A code that allows person to personalize a story

---
+++ first_glance.py
@@ -0,0 +1,8 @@
+century =  input("Which century you want to input? \n")
+country = input("Select the place this incident will happen?\n")
+mainchar = input("Select the name of the main character-\n")
+region = input("Select the region in which throne will be selected? \n")
+genre = input("Select the generation- \n")
+
+print("It was in the second decade of the " + century + "  Century, after the Great Plague had devastated  " +country.upper()+ ", that ," +mainchar+" nicknamed also the Wise, sat on the "+region.upper()+ " throne. The Mortal Sickness had swept away the entire Royal Family, unto the "+genre+ "generations, and thus it came to pass that "+mainchar+" the Fourteenth of Saxe-Drachsen-Wachtelstein, who had stood thirtieth in the order of succession, found himself one day ruler of the "+region+ " dominions within and beyond the seas. He was one of the unexpected things that happen in politics, and he happened with great thoroughness. In many ways he was the most progressive monarch who had sat on an important throne; before people knew where they were, they were somewhere else. Even his Ministers, progressive though they were by tradition, found it difficult to keep pace with his legislative suggestions.")
+
